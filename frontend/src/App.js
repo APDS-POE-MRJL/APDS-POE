08f0a4774f54567392de25c3bb56d59bbbbd7b0b
Matt Stewart <103989501+ItStewball@users.noreply.github.com>
2024-09-30 08:36:54 +0200

    Initial commit
    This includes the default application

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the initial build
+        </p>
+        <a>
+          We're going to build the frontend here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
